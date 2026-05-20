4e69a84c7c29b12308fe9fec5110a5a28a51dad8
Malte <development@malteans.de>
2026-05-20 10:23:40 +0200

    fix: refactoring fixed... but ai did an okay job.

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,14 +2,10 @@ plugins {
     alias(libs.plugins.kotlinMultiplatform)
     alias(libs.plugins.android.kotlin.multiplatform.library)
     alias(libs.plugins.compose.multiplatform)
-    alias(libs.plugins.compose.compiler)
+    alias(libs.plugins.kotlin.compose)
 }
 
 kotlin {
-
-    // Target declarations - add or remove as needed below. These define
-    // which platforms this KMP module supports.
-    // See: https://kotlinlang.org/docs/multiplatform-discover-project.html#targets
     androidLibrary {
         namespace = "de.malteans.datastore"
         compileSdk = 36
@@ -18,13 +14,6 @@ kotlin {
 
     jvm("desktop")
 
-    // For iOS targets, this is also where you should
-    // configure native binary output. For more information, see:
-    // https://kotlinlang.org/docs/multiplatform-build-native-binaries.html#build-xcframeworks
-
-    // A step-by-step guide on how to include this library in an XCode
-    // project can be found here:
-    // https://developer.android.com/kotlin/multiplatform/migrate
     listOf(
         iosX64(),
         iosArm64(),
@@ -39,32 +28,27 @@ kotlin {
     sourceSets {
         val desktopMain by getting
 
-        androidMain {
-            dependencies {
-                implementation(libs.compose.ui.tooling.preview)
-                implementation(libs.androidx.activity.compose)
-            }
+        androidMain.dependencies {
+            implementation(libs.compose.ui.tooling.preview)
+            implementation(libs.androidx.activity.compose)
         }
 
-        commonMain {
-            dependencies {
-                implementation(libs.kotlin.stdlib)
+        commonMain.dependencies {
+            implementation(libs.kotlin.stdlib)
 
-                implementation(libs.bundles.compose)
-                implementation(libs.compose.material3)
+            implementation(libs.bundles.compose)
+            implementation(libs.compose.material3)
 
-                api(libs.datastore)
-                api(libs.datastore.preferences)
-            }
+            api(libs.datastore)
+            api(libs.datastore.preferences)
         }
 
         desktopMain.dependencies {
             implementation(compose.desktop.currentOs)
         }
 
-        iosMain {
-            dependencies {
-            }
+        iosMain.dependencies {
+
         }
     }
 }
